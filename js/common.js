90b4e92387af23c6f35307b2f4dadb7d5d502689
whats0n <mrdvv@mail.ru>
2015-02-13 14:13:10 +0200

    done

--- js/common.js
+++ js/common.js
@@ -14,48 +14,7 @@ head.ready(function() {
 	// $(window).scroll(function(){
 	//     scrollFixedElements()
 	// });
-
-
-	//select
-	$(document).ready(function() {
-	    $(document).click(function() {
-	        $(".js-select-list").hide();
-	        $(".js-select").removeClass("is-active");
-	    });
-	    function selectList() {
-	        var select = $(".js-select");
-	        var select_list = $(".js-select-list");
-	        $("body").on("click", ".js-select", function(event){
-	            if ($(this).hasClass("is-active")) {
-	                select.removeClass("is-active");
-	                select_list.hide();
-	            }
-	            else {
-	                select.removeClass("is-active");
-	                select_list.hide();
-	                $(this).find(".js-select-list").show();
-	                $(this).addClass("is-active");
-	            }
-	            event.stopPropagation();
-	        });
-	        $("body").on("click", ".js-select-list li", function(event){
-	            var id = $(this).attr("data-id");
-	            var text = $(this).text();
-	            $(this).parents(".js-select").find(".js-select-text").text(text);
-	            $(this).parents(".js-select").find(".js-select-input").val(id);
-	            $(this).parent().hide();
-	            $(this).parents(".js-select").removeClass("is-active");
-	            event.stopPropagation();
-	        });
-	    }  
-	    selectList();
-	    $("body").on("click", ".js-select", function(event){
-	        event.stopPropagation();
-	    });
-	    $('.select__list li').on('click', function() {
-	    	$('.select__text').addClass('is-active');
-	    });
-	});
+	var clickEventType=((document.ontouchstart!==null)?'click':'touchstart');
 
 	//popups
 	$('.js-popup-btn').on("click", function(){
@@ -69,8 +28,17 @@ head.ready(function() {
 	});
 	$('.js-popup-close').on("click", function(){
 		$(".js-popup").removeClass('is-active');
+		$(".js-popup-in").removeClass('is-active');
 		return false;
 	});
+	$('.overlay').on("click", function(){
+		$(".js-popup").removeClass('is-active');
+		$('.js-popup-in').removeClass('is-active');
+		return false;
+	});
+
+
+
 	$('.js-menu-btn').on("click", function(){
 		if ($('.js-menu').hasClass('is-active')) {
 			$(".js-menu").removeClass('is-active');
@@ -85,4 +53,52 @@ head.ready(function() {
 		return false;
 	});
 
+	$(".js-form-select").change(function() {
+		$(this).parent('.js-form-choice').find('.js-form-input').val($(this).val());
+	});
+
+	$('.js-form-box input').focusin(function() {
+		$(this).parent().addClass('is-active');
+	});
+	$('.js-form-box input').focusout(function() {
+		$(this).parent().removeClass('is-active');
+	});
+
+	$('.js-popup-open').click(function(event) {
+		attr = $(this).attr('href');
+		$('#pop1, #pop2, #pop3, #pop4, #pop5, #pop6, #pop7').removeClass('is-active');
+		$(' '+attr).addClass('is-active');
+		if ($('.js-popup').hasClass('is-active')) {
+			$(".js-popup").removeClass('is-active');
+		}
+		else {
+			$(".js-popup").addClass('is-active');
+		}		
+		return false;
+	});
+
+	//drop
+	$('.js-drop-btn').on('click', function() {
+		$('.js-drop-items').toggleClass('is-active');
+		return false;
+	});
+	$('body').on('click', function(event) {
+		$('.js-drop-items').removeClass('is-active');
+	});
+	$('.js-drop-items li').on('click', function() {
+		$(this).parents('.js-drop-choice').find('.js-drop-input').val($(this).data("value"));
+		$(this).parents('.js-drop-items').removeClass('is-active');
+	});
+	$("body").on("click", ".js-drop-items", function(event){
+        event.stopPropagation();
+    });
+	//tabs
+    $('.tabs a').click(function(event) {
+    	$('.tabs a').removeClass('is-active');
+    	$(this).addClass('is-active');
+		attr = $(this).attr('href');
+		$('#tab1,#tab2').addClass('is-hidden');
+		$(' '+attr).removeClass('is-hidden');
+		return false;
+	});
 });
